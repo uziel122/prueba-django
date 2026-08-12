7e8c70d0d27715f769aa9d807ce1657711e841f6
uziel122 <herrerauziel38@gmail.com>
2026-08-12 16:45:56 -0600

    Configurar SQLite para PythonAnywhere

--- prueba/settings.py
+++ prueba/settings.py
@@ -128,11 +128,7 @@ CKEDITOR_CONFIGS ={
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'prueba',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
